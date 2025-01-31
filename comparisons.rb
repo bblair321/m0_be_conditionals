af12cba4fc593eb08f448d4f3424d31ab97cd290
Brady Blair <bblair321@yahoo.comm>
2025-01-31 12:21:37 -0800

    Completed part 3

--- comparisons.rb
+++ comparisons.rb
@@ -54,10 +54,84 @@ puts "is number_students less than or equal to 21?", number_students <= 21
 
 puts 4 < 9
 #YOU DO: Explain.
+# puts 4<9 is checking if integer 4 is less than 9. Then it will use puts to print the result of the comparison which is true.
+# 4 is less than 9 so it will print true.
+
+dogs = 2
+cats = 3
+puts dogs > cats
+# YOU DO: Explain.
+# the variable dogs is assigned integer 2. 
+# the variable cats is assigned integer 3.
+# puts dogs > cats is checking if dogs is greater than cats. 
+# Then it will use puts to print the result of the comparison which is false.
+# 2 is not greater than 3 so it will print false.
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+# books is assigned integer 3.
+# puts 4 < books is checking if 4 is less than books.
+# Then it will use puts to print the result of the comparison which is false.
+# 4 is not less than 3 so it will print false.
+
+friends = 6
+siblings = 2
+puts friends > siblings
+# YOU DO: Explain.
+# friends is assigned integer 6.
+# siblings is assigned integer 2.
+# puts friends > siblings is checking if friends is greater than siblings.
+# Then it will use puts to print the result of the comparison which is true.
+# 6 is greater than 2 so it will print true.
+
+attendees = 9
+meals = 8
+puts attendees != meals
+# YOU DO: Explain.
+# attendees is assigned integer 9.
+# meals is assigned integer 8.
+# puts attendees != meals is checking if attendees is not equal to meals.
+# Then it will use puts to print the result of the comparison which is true.
+# 9 is not equal to 8 so it will print true.
+
+#-------------------
+# PART 3: Logical Operators
+#-------------------
+
+is_hungry = true
+finished_homework = false
+
+# EXAMPLE:
+# Determine if the user is hungry and has completed their homework
+puts is_hungry && finished_homework
+# this should print: false
+# is_hungry is true and finished_homework is false so the result is false.
+
+loves_to_play = true
+loves_dog_park = false
+loves_treats = true
+age = 1
+
+# Determine if the dog loves to play and loves treats
+puts loves_to_play && loves_treats
+# this should print: true
+# loves_to_play is true and loves_treats is true so the result is true.
+
+# Determine if the dog loves to play and loves the dog park
+puts loves_to_play && loves_dog_park
+# this should print: false
+# loves_to_play is true and loves_dog_park is false so the result is false.
+
+# Determine if the dog loves to play or loves the dog park
+puts loves_to_play || loves_dog_park
+# this should print: true
+# loves_to_play is true and loves_dog_park is false so the result is true.
+
+# Determine if the dog loves to play and is a puppy
+# Assume that a dog is a puppy if it is less than 2 years old.
+puts loves_to_play && age <
+
 
 friends = 6
 siblings = 2
