b9dc5abb2fbb1b269b911d213b98287b94d2ba77
Brady Blair <bblair321@yahoo.comm>
2025-01-31 13:32:53 -0800

    Completed the medium and mild code challenge

---
+++ code_challenges/mild_code_challenge.rb
@@ -0,0 +1,11 @@
+## Mild
+
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+Integerstore = 2
+if Integerstore.even?
+  puts "even"
+else
+  puts "odd"
+
+end
